ad4b036110975c5d16b38443bc804a5ce981f234
Stylianos <styltsars@gmail.com>
2026-02-06 19:24:30 +0200

    chore: Migrated helpers from old API config, deleted old API config
    - TODO: Old nodes to be migrated into new type of node!
    - Complete the logger node

--- node-red-contrib-humaine/nodes/shared/helpers.ts
+++ node-red-contrib-humaine/nodes/shared/helpers.ts
@@ -2,6 +2,10 @@ import { Node } from "node-red";
 import { HaicConfigNode } from "../haic-config/modules/types";
 import { z } from "zod";
 
+// CONSTANTS FOR THE UTILITY FUNCTIONS
+const RETRIES_NUM = 3;
+const RETRIES_INTERVAL = 1000;
+
 const recordSchema = z.record(z.string(), z.string());
 
 export const getStringsRecord = (
@@ -31,4 +35,38 @@ export const getStringsRecord = (
 export const isHaicConfigNode = (node: Node): node is HaicConfigNode => {
     if (!node) return false;
     return "OpenAPI" in node;
+};
+
+export const isEmpty = (obj: any): boolean => {
+    for (const prop in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+export const mergeObjects = (a: any, b: any) => ({
+    ...((a ?? {}) || {}),
+    ...((b ?? {}) || {}),
+});
+
+export const delay = (time: number) => {
+    return new Promise((res) => {
+        setTimeout(res, time);
+    });
+};
+
+export const retryWithDelay = async (
+    fn,
+    retries = RETRIES_NUM,
+    interval = RETRIES_INTERVAL,
+) => {
+    try {
+        return await fn();
+    } catch (err) {
+        if (retries <= 0) throw err;
+        await delay(interval);
+        return retryWithDelay(fn, retries - 1, interval);
+    }
 };
